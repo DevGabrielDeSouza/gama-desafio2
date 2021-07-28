10c9805e604591885c5d86133e76188e568af899
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-07-28 00:35:51 -0300

    Responsive sign up form

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -14,24 +14,24 @@ import { Container } from './style';
 const Signup: React.FC/*<{submitForm: any}>*/ = (/*{ submitForm }*/) => {
 	//const paperStyle = { padding: '4vmin 2vmin', display: "inline-block", maxWidth: 450, width: "80%", margin: "20px auto" }
 	const paperStyle = {
-		padding: '4vmin 2vmin 6vmin 2vmin',
-		display: "inline-block", 
-		position: "absolute" as "absolute", 
-		top: "6%", 
-		bottom: "6%",
-		minHeight: "500px",
+		padding: '4vmin 2vmin 4vmin 2vmin',
+		display: "inline-block",
+		//minHeight: "300px",
 		maxWidth: "80%",
+		margin: "10vh 0vh 0vh 0vh",
 	}
 	const headerStyle = { margin: 0, width: "100%", textAlign: "center" as "center", fontSize: "max(200%, 6vmin)"}
-	const descStyle = { textAlign: "center" as "center", margin: "3.2vh 0 1vh 0", fontSize: "max(90%, 2vmin)" }
-	const inputRegionStyle = { margin: "2vh 20px 3vh 20px" }
+	const descStyle = { textAlign: "center" as "center", margin: "10px 0 1vh 0", fontSize: "max(90%, 2vmin)" }
+	const inputRegionStyle = { width: "90%",}
 	const inputStyle = { height: "max(200%, 2vmin)" }
 	const marginTop = {
 		//background: "linear-gradient(45deg, #9013FE 15%, #50E3C2 90%)",
 		//background: "linear-gradient(45deg, #512DA8 15%, #546E7A 90%)",
 		minWidth: "100%",
-		minHeight: "max(620px, 100vh)",
 		display: "flex",
+		flexDirection: "column" as "column",
+		alignItems: "center" as "center",
+		position: "relative" as "relative",
 		justifyContent: "center",
 	}
 	const bg = {
@@ -41,12 +41,26 @@ const Signup: React.FC/*<{submitForm: any}>*/ = (/*{ submitForm }*/) => {
 		minHeight: "max(620px, 100vh)",
 		position: "fixed" as "fixed",
 	}
-	const acceptStyle = { margin: "20px" }
-	const buttonStyle = { width: "60%", margin: "10px 10px 10px 10px " }
+	const acceptStyle = { margin: "20px", width: "100%"}
+	const buttonStyle = { width: "150px", height: "100%"}
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+	const warningStyle = { color: "red", margin: "0.5vh 0px 0px 0px", fontSize: "80%", /*display: "none"*/}
+
+	const formStyle = {
+		display: "flex",
+		flexDirection: "column" as "column",
+		alignItems: "center",
+	}
+
+	const handlePasswordConfirm = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
 		console.log(e.target.value)
 	}
+
+	const handlePassword = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		console.log(e.target.value)
+	}
+
+	const emptySpaceStyle = { width: "5px", height: "10vh", /*background: "red"*/}
 
 	return (
 		<Container>
@@ -59,18 +73,22 @@ const Signup: React.FC/*<{submitForm: any}>*/ = (/*{ submitForm }*/) => {
 							<Typography style={descStyle} variant='caption' gutterBottom>Cadastre-se e descubra vários jogos e ferramentas!</Typography>
 						</Grid>
 					</div>
-					<form>
+					<form style={formStyle}>
 						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Name' placeholder="Enter your name" />
+							<TextField style={inputStyle} fullWidth label='Nome' placeholder="Insira seu nome" />
+							<span style={warningStyle}>Insira um nome válido</span>
 						</div>
 						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Email' placeholder="Enter your email" />
+							<TextField style={inputStyle} fullWidth label='E-mail' placeholder="Insira seu e-mail" />
+							<span style={warningStyle}>Insira um e-mail válido</span>
 						</div>
 						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Password' placeholder="Enter your password" onChange={()=>console.log("HOI")}/>
+							<TextField style={inputStyle} fullWidth label='Password' placeholder="Insira uma senha" onChange={handlePassword}/>
+							<span style={warningStyle}>Senha muito curta (mínimo de 8 dígitos)</span>
 						</div>
 						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Confirm Password' placeholder="Confirm your password" onChange={handleChange}/>
+							<TextField style={inputStyle} fullWidth label='Confirm Password' placeholder="Confirm your password" onChange={handlePasswordConfirm}/>
+							<span style={warningStyle}>Senhas diferentes</span>
 						</div>
 						<FormControlLabel
 							control={<Checkbox name="checkedA" />}
@@ -80,6 +98,7 @@ const Signup: React.FC/*<{submitForm: any}>*/ = (/*{ submitForm }*/) => {
 						<Button type='submit' variant='contained' color='primary' style={buttonStyle}>Sign up</Button>
 					</form>
 				</Paper>
+				<div style={emptySpaceStyle}></div>
 			</Grid>
 		</Container>
 	);
