f64d7c3cdc891c79c0194d247c10d3c844c72eec
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-07-28 03:03:28 -0300

    Sign up form validation

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,96 +1,101 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-import { Container } from './style';
+import { SignupStyle } from './style';
+import SignUpValidation from './SignUpValidation';
 
 const Signup: React.FC/*<{submitForm: any}>*/ = (/*{ submitForm }*/) => {
-	const paperStyle = {
-		padding: '4vmin 2vmin 4vmin 2vmin',
-		display: "inline-block",
-		maxWidth: "80%",
-		margin: "8vh 0vh 0vh 0vh",
-	}
-	const headerStyle = { margin: 0, width: "100%", textAlign: "center" as "center", fontSize: "max(200%, 6vmin)"}
-	const descStyle = { textAlign: "center" as "center", margin: "10px 0 1vh 0", fontSize: "max(90%, 2vmin)" }
-	const inputRegionStyle = { width: "90%",}
-	const inputStyle = { height: "max(200%, 2vmin)" }
-	const marginTop = {
-		minWidth: "100%",
-		display: "flex",
-		flexDirection: "column" as "column",
-		alignItems: "center" as "center",
-		position: "relative" as "relative",
-		justifyContent: "center",
-	}
-	const bg = {
-		background: "linear-gradient(225deg, #7953d2 0%, #512DA8 30%, #546E7A 85%)",
-		minWidth: "100%",
-		minHeight: "max(620px, 100vh)",
-		position: "fixed" as "fixed",
-	}
-	const acceptStyle = { margin: "20px", width: "100%"}
-	const buttonStyle = { width: "150px", height: "100%"}
 
-	const warningStyle = { color: "red", margin: "0.5vh 0px 0px 0px", fontSize: "80%", /*display: "none"*/}
+	const [showNameWarning, setNameWarning] = useState(false);
+	const [showEmailWarning, setEmailWarning] = useState(false);
+	const [showPassWarning, setPassWarning] = useState(false);
+	const [disableConfirmPass, setDisableConfirmPass] = useState(true);
+	const [showConfirmPassWarning, setConfirmPassWarning] = useState(false);
 
-	const formStyle = {
-		display: "flex",
-		flexDirection: "column" as "column",
-		alignItems: "center",
+	const [passwordData, setPasswordData] = useState<string>();
+
+
+	const handleName = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		let validation = SignUpValidation.validateName(e.target.value);
+		setNameWarning(!validation);
+
+		if (validation) {
+			
+		}
+
 	}
 
-	const handlePasswordConfirm = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		console.log(e.target.value)
+	const handleEmail = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		let validation = SignUpValidation.validateEmail(e.target.value);
+		setEmailWarning(!validation);
+
+		if (validation) {
+
+		}
 	}
 
 	const handlePassword = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		console.log(e.target.value)
+		setPasswordData(e.target.value);
+
+		let validation = SignUpValidation.validatePassword(e.target.value);
+		setPassWarning(!validation);
+		setDisableConfirmPass(!validation);
 	}
 
-	const emptySpaceStyle = { width: "5px", height: "5vh", /*background: "red"*/}
+	const handlePasswordConfirm = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		setConfirmPassWarning(!(e.target.value == passwordData));
+	}
 
 	return (
-		<Container>
-			<div style={bg}></div>
-			<Grid style={marginTop} >
-				<Paper elevation={20} style={paperStyle}>
+		<>
+			<div style={SignupStyle.bg}></div>
+			<Grid style={SignupStyle.marginTop} >
+				<Paper elevation={20} style={SignupStyle.paperStyle}>
 					<div>
-						<Grid item container justify="center">
-							<h2 style={headerStyle}>Cadastro</h2>
-							<Typography style={descStyle} variant='caption' gutterBottom>Cadastre-se e descubra vários jogos e ferramentas!</Typography>
+						<Grid item container justifyContent="center">
+							<h2 style={SignupStyle.headerStyle}>Cadastro</h2>
+							<Typography style={SignupStyle.descStyle} variant='caption' gutterBottom>Cadastre-se e descubra vários jogos e ferramentas!</Typography>
 						</Grid>
 					</div>
-					<form style={formStyle}>
-						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Nome' placeholder="Insira seu nome" />
-							<span style={warningStyle}>Insira um nome válido</span>
+					<form style={SignupStyle.formStyle}>
+						<div style={SignupStyle.inputRegionStyle}>
+							<TextField style={SignupStyle.inputStyle} fullWidth label='Nome' placeholder="Insira seu nome" onChange={handleName}/>
+							{
+								showNameWarning?<span style={SignupStyle.warningStyle}>Insira um nome válido</span>:null
+							}
 						</div>
-						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='E-mail' placeholder="Insira seu e-mail" />
-							<span style={warningStyle}>Insira um e-mail válido</span>
+						<div style={SignupStyle.inputRegionStyle}>
+							<TextField style={SignupStyle.inputStyle} fullWidth label='E-mail' placeholder="Insira seu e-mail" onChange={handleEmail}/>
+							{
+								showEmailWarning?<span style={SignupStyle.warningStyle}>Insira um e-mail válido</span>:null
+							}
 						</div>
-						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Password' placeholder="Insira uma senha" onChange={handlePassword}/>
-							<span style={warningStyle}>Senha muito curta (mínimo de 8 dígitos)</span>
+						<div style={SignupStyle.inputRegionStyle}>
+							<TextField style={SignupStyle.inputStyle} fullWidth label='Password' placeholder="Insira uma senha" onChange={handlePassword}/>
+							{
+								showPassWarning?<span style={SignupStyle.warningStyle}>Senha muito curta (mínimo de 8 dígitos)</span>:null
+							}
 						</div>
-						<div style={inputRegionStyle}>
-							<TextField style={inputStyle} fullWidth label='Confirm Password' placeholder="Confirm your password" onChange={handlePasswordConfirm}/>
-							<span style={warningStyle}>Senhas diferentes</span>
+						<div style={SignupStyle.inputRegionStyle}>
+							<TextField disabled={disableConfirmPass} style={SignupStyle.inputStyle} fullWidth label='Confirm Password' placeholder="Confirm your passwordData" onChange={handlePasswordConfirm}/>
+							{
+								showConfirmPassWarning?<span style={SignupStyle.warningStyle}>Senhas diferentes</span>:null
+							}
 						</div>
 						<FormControlLabel
 							control={<Checkbox name="checkedA" />}
 							label="I accept the terms and conditions."
-							style={acceptStyle}
+							style={SignupStyle.acceptStyle}
 						/>
-						<Button type='submit' variant='contained' color='primary' style={buttonStyle}>Sign up</Button>
+						<Button type='submit' variant='contained' color='primary' style={SignupStyle.buttonStyle}>Sign up</Button>
 					</form>
 				</Paper>
-				<div style={emptySpaceStyle}></div>
+				<div style={SignupStyle.emptySpaceStyle}></div>
 			</Grid>
-		</Container>
+		</>
 	);
 }
 
--- src/components/Signup/style.tsx
+++ src/components/Signup/style.tsx
@@ -1,13 +1,40 @@
-import styled from 'styled-components';
 
-export const Container = styled.div`
 
-	html{
-		minHeight: 100%;/* make sure it is at least as tall as the viewport */
-		height: 100%;
-		position: relative;
+export class SignupStyle {
+	static paperStyle = {
+		padding: '4vmin 2vmin 4vmin 2vmin',
+		display: "inline-block",
+		maxWidth: "80%",
+		margin: "8vh 0vh 0vh 0vh",
 	}
-	body{
-		height: 100%; /* force the BODY element to match the height of the HTML element */
+	static headerStyle = { margin: 0, width: "100%", textAlign: "center" as "center", fontSize: "max(200%, 6vmin)"}
+	static descStyle = { textAlign: "center" as "center", margin: "10px 0 1vh 0", fontSize: "max(90%, 2vmin)" }
+	static inputRegionStyle = { width: "90%",}
+	static inputStyle = { height: "max(200%, 2vmin)" }
+	static marginTop = {
+		minWidth: "100%",
+		display: "flex",
+		flexDirection: "column" as "column",
+		alignItems: "center" as "center",
+		position: "relative" as "relative",
+		justifyContent: "center",
 	}
-`
+	static bg = {
+		background: "linear-gradient(225deg, #7953d2 0%, #512DA8 30%, #546E7A 85%)",
+		minWidth: "100%",
+		minHeight: "max(620px, 100vh)",
+		position: "fixed" as "fixed",
+	}
+	static acceptStyle = { margin: "20px", width: "100%"}
+	static buttonStyle = { width: "150px", height: "100%"}
+
+	static warningStyle = { color: "red", margin: "0.5vh 0px 0px 0px", fontSize: "80%", /*display: "none"*/}
+
+	static formStyle = {
+		display: "flex",
+		flexDirection: "column" as "column",
+		alignItems: "center",
+	}
+
+	static emptySpaceStyle = { width: "5px", height: "5vh", /*background: "red"*/ }
+}
